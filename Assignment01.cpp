933762c6afbd7e2b422f7fdd0d9d5465397cd7fa
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2025-01-08 04:55:25 +0000

    DFS and BFS Recursive in Graph

---
+++ Assignment01.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bfsRecursive(int n, vector<vector<int>>& edgelist, queue<int>& q, unordered_set<int>& visited) {
+    visited.insert(n);
+    cout << n << " ";
+
+    for (int neighbor : edgelist[n]) {
+        if (visited.find(neighbor) == visited.end()) {
+            q.push(neighbor);
+            visited.insert(neighbor);
+        }
+    }
+ 
+    if (!q.empty()) {
+        int next_node = q.front();
+        q.pop();
+        bfsRecursive(next_node, edgelist, q, visited);
+    }
+}
+
+void dfsRec(vector<vector<int>> &adj, vector<bool> &visited, int s){
+    visited[s] = true;
+    cout << s << " ";
+
+    for (int i : adj[s])
+        if (visited[i] == false)
+            dfsRec(adj, visited, i);
+}
+
+
+int main() {
+    // Define number of nodes
+    int nodes = 5;
+
+    // Example: Edge list representation of an undirected graph
+    vector<vector<int>> edgelist(nodes);
+    edgelist[0] = {1, 2};
+    edgelist[1] = {0, 3, 4};
+    edgelist[2] = {0};
+    edgelist[3] = {1};
+    edgelist[4] = {1};
+
+    // Initialize a queue for BFS and a set for tracking visited nodes
+    queue<int> q;
+    unordered_set<int> visited;
+
+    cout << "BFS Traversal: ";
+    bfsRecursive(0, edgelist, q, visited);
+    cout << endl;
+
+    vector<bool> visit(nodes,0);
+    cout<<"DFS Tracersal: ";
+    dfsRec(edgelist,visit,0);
+    cout<<endl;
+    return 0;
+}
